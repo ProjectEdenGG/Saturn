853eadafe741ec8a865688f1449f5f242defccbd
Griffin Kubesa <pugabyte@projecteden.gg>
2021-05-04 13:48:28 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 cd /home/minecraft/git/Saturn/
 
-rm BearNationResourcePack.zip
-rm /srv/http/cdn.bnn.gg/BearNationResourcePack.zip
+rm ResourcePack.zip
+rm /srv/http/cdn/ResourcePack.zip
 git reset --hard origin/main
 git pull
-zip -r BearNationResourcePack.zip . -x '*.git*' -x 'deploy.sh' -x 'update.bat' -q
-mv BearNationResourcePack.zip /srv/http/cdn.bnn.gg
-chown www-data:www-data /srv/http/cdn.bnn.gg -R
-
+zip -r ResourcePack.zip . -x '*.git*' -x 'deploy.sh' -x 'update.bat' -q
+mv ResourcePack.zip /srv/http/cdn
+chown www-data:www-data /srv/http/cdn -R
