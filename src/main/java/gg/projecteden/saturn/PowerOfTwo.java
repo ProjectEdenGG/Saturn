b66ada5621d4efd9eeeed10744df5e44738b6371
GriffinCodes <griffin@projecteden.gg>
2023-12-22 18:02:29 -0600

    Add resize script

--- src/main/java/gg/projecteden/saturn/PowerOfTwo.java
+++ src/main/java/gg/projecteden/saturn/PowerOfTwo.java
@@ -4,6 +4,7 @@ import gg.projecteden.saturn.utils.ImageUtils;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.nio.file.Files;
@@ -25,9 +26,6 @@ public class PowerOfTwo {
 			return true;
 
 		if (!uri.contains("textures/projecteden")) // ignore default textures
-			return true;
-
-		if (uri.contains("textures/projecteden/font")) // ignore font textures
 			return true;
 
 		return false;
@@ -91,5 +89,51 @@ public class PowerOfTwo {
 		System.out.println();
 		System.out.println("Total: " + textures.size());
 		System.out.println("Not pow 2: " + notPower2);
+	}
+
+	@Test
+	@SneakyThrows
+	void resize() {
+		String folderName = "assets/minecraft/textures/projecteden/font";
+		Path folderPath = Paths.get(folderName);
+
+		try (var walker = Files.walk(folderPath)) {
+			walker.forEach(path -> {
+				try {
+					final String uri = path.toUri().toString();
+
+					if (!uri.endsWith(".png")) {
+						return;
+					}
+
+					var texture = ImageUtils.read(path.toFile());
+					int height = texture.getHeight();
+					int width = texture.getWidth();
+					System.out.println(height + " " + width + " " + uri);
+
+					if (isPowerOfTwo(height) && isPowerOfTwo(width))
+						return;
+
+					int newHeight = 2;
+					int newWidth = 2;
+
+					while (height > newHeight)
+						newHeight *= 2;
+
+					while (width > newWidth)
+						newWidth *= 2;
+
+					System.out.println(newHeight + " " + newWidth);
+
+					final BufferedImage resized = ImageUtils.newImage(newWidth, newHeight);
+					final Graphics2D graphics = resized.createGraphics();
+					graphics.drawImage(texture, 0, 0, null);
+					ImageUtils.write(resized, path.toFile());
+				} catch (Exception ex) {
+					System.out.println(path.getFileName().toString());
+					ex.printStackTrace();
+				}
+			});
+		}
 	}
 }
